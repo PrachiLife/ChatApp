3cfdffd8bb82822a89892f8bff75734744d958a4
PrachiLife <prachi23january@gmail.com>
2023-04-23 22:30:07 +0530

    added dependencies

---
+++ src/components/LoginComponent.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-container>
+    Chat App   
+  </v-container>
+</template>
+
+<script>
+
+export default {
+  name: 'LoginComponent',
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,18 @@
 <template>
-  <HelloWorld />
+  <LoginComponent />
 </template>
 
 <script>
 import { defineComponent } from 'vue';
 
 // Components
-import HelloWorld from '../components/HelloWorld.vue';
+import LoginComponent from '../components/LoginComponent.vue';
 
 export default defineComponent({
   name: 'HomeView',
 
   components: {
-    HelloWorld,
+    LoginComponent,
   },
 });
 </script>
